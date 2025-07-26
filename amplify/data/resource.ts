47564f2f23cd2790a7bc9b1eecf351110af28f77
土肥　大樹 <bigtree19940729@gmail.com>
2025-07-27 00:48:27 +0900

    added ai story generator

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,6 +10,22 @@ const schema = a.schema({
   Todo: a.model({
     content: a.string(),
   }).authorization(allow => [allow.owner()]),
+    
+  generateRecipe: a.generation({
+    aiModel: a.ai.model('Claude 3.5 Sonnet'),
+    systemPrompt: 'You are a helpful assistant that generates stories.',
+  })
+  .arguments({
+    description: a.string(),
+  })
+  .returns(
+    a.customType({
+      name: a.string(),
+      ingredients: a.string().array(),
+      instructions: a.string(),
+    })
+  )
+  .authorization((allow) => allow.authenticated()),
 });
 
 export type Schema = ClientSchema<typeof schema>;
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import * as React from 'react';
 import type { Schema } from "../amplify/data/resource";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator, Flex, TextAreaField, Loader, Text, View, Button } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
+import { useAIGeneration } from "./client";
 
 const client = generateClient<Schema>();
 
@@ -9,6 +11,14 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   const { user, signOut } = useAuthenticator();
+
+  const [description, setDescription] = React.useState("");
+  const [{ data, isLoading }, generateRecipe] =
+    useAIGeneration("generateRecipe");
+
+  const handleClick = async () => {
+    generateRecipe({ description });
+  };
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -43,7 +53,33 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
-      <button onClick={signOut}>Sign out</button>
+      <Flex direction="column">
+      <Flex direction="row">
+        <TextAreaField
+          autoResize
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          label="Description"
+        />
+        <Button onClick={handleClick}>Generate story</Button>
+      </Flex>
+      {isLoading ? (
+        <Loader variation="linear" />
+      ) : (
+        <>
+          <Text fontWeight="bold">{data?.name}</Text>
+          <View as="ul">
+            {data?.ingredients?.map((ingredient) => (
+              <View as="li" key={ingredient}>
+                {ingredient}
+              </View>
+            ))}
+          </View>
+          <Text>{data?.instructions}</Text>
+        </>
+      )}
+    </Flex>
+    <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
